a93bed6dc6428717ca9e37841ed3f0b6efaec708
Marco Almeida <marcoalmeida@webdados.pt>
2025-05-22 14:29:17 +0100

    10.4.1

--- multibanco_ifthen_for_woocommerce.php
+++ multibanco_ifthen_for_woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name:          Multibanco, MB WAY, Credit card, Apple Pay, Google Pay, Payshop, Cofidis Pay, and PIX (ifthenpay) for WooCommerce
  * Plugin URI:           https://www.webdados.pt/wordpress/plugins/multibanco-ifthen-software-gateway-woocommerce-wordpress/
  * Description:          Secure WooCommerce payments with Multibanco, MB WAY, Credit card, Apple Pay, Google Pay, Payshop, Cofidis, and PIX via ifthenpay’s payment gateway.
- * Version:              10.4.0
+ * Version:              10.4.1
  * Author:               Naked Cat Plugins (by Webdados)
  * Author URI:           https://nakedcatplugins.com
  * Text Domain:          multibanco-ifthen-software-gateway-for-woocommerce
@@ -11,7 +11,7 @@
  * Tested up to:         6.8
  * Requires PHP:         7.2
  * WC requires at least: 7.1
- * WC tested up to:      9.8
+ * WC tested up to:      9.9
  * Requires Plugins:     woocommerce
  * License:              GPLv3
  **/
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: multibanco, mbway, payshop, card, cofidis
 Requires at least: 5.8
 Tested up to: 6.8
 Requires PHP: 7.2
-Stable tag: 10.4.0
+Stable tag: 10.4.1
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
@@ -254,6 +254,10 @@ You can report any security bugs found in the source code of this plugin through
 Sure. Go to [GlotPress](https://translate.wordpress.org/projects/wp-plugins/multibanco-ifthen-software-gateway-for-woocommerce) and help us out.
 
 == Changelog ==
+
+= 10.4.1 - 2025-05-22 =
+* [FIX] Check for WooCommerce version before declaring HPOS and Blocks checkout compatibility
+* [DEV] Tested with WordPress 6.9-alpha-60243 and WooCommerce 9.9.0-beta.1
 
 = 10.4.0 - 2025-05-03 =
 * [TWEAK] New payment method notices are now only shown every 90 days for each admin or shop manager user (they can also be dismissed by returning `true` to the `multibanco_ifthen_hide_newmethod_notifications` filter)
